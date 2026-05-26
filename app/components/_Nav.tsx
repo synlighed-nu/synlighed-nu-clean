dd91f2edc339416331b8145d111373db0540d630
synlighed-nu <github@fridan.dk>
2026-05-26 07:31:10 +0200

    Update _Nav.tsx

--- app/components/_Nav.tsx
+++ app/components/_Nav.tsx
@@ -12,7 +12,7 @@ export default function Nav() {
         
         <div className="flex justify-between items-center">
           <Link href="/" className="text-2xl font-semibold tracking-tighter text-[#002B5B]">
-            @SynlighedNu
+            @SynlighedNu TEST
           </Link>
 
           <button
